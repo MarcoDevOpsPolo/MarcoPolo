3a36102ff8a58cf3c34f5f1a91830f154777eaa4
Stefanov Emilia <stemilia92@gmail.com>
2024-11-26 13:29:29 +0300

    associate private subnet to the route table

---
+++ associate_subnet_to_rt.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+vpcId=$(aws ec2 describe-vpcs --filters "Name=tag:Name,Values=my-vpc-stemilia" --query "Vpcs[0].VpcId" --output text)
+subnetId=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$vpcId" --query "Subnets[0].SubnetId" --output text)
+rtId=$(aws ec2 describe-route-tables --filters "Name=vpc-id,Values=$vpcId" --query "RouteTables[0].RouteTableId" --output text)
+
+aws ec2 associate-route-table --route-table-id "$rtId" --subnet-id "$subnetId"
