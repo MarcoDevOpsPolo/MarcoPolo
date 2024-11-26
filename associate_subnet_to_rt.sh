c0388ee44e921baf2b87f017bf118891fff9401a
Stefanov Emilia <stemilia92@gmail.com>
2024-11-26 16:51:42 +0300

    refactor associate_subnet_to_rt.sh to make it available both for public or private prefixed subnets and rts

--- associate_subnet_to_rt.sh
+++ associate_subnet_to_rt.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+namePart="$1" #private or public
+
 vpcId=$(aws ec2 describe-vpcs --filters "Name=tag:Name,Values=my-vpc-stemilia" --query "Vpcs[0].VpcId" --output text)
-subnetId=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$vpcId" "Name=tag:Name,Values=my-private-subnet-stemilia" --query "Subnets[0].SubnetId" --output text)
-rtId=$(aws ec2 describe-route-tables --filters "Name=vpc-id,Values=$vpcId" --query "RouteTables[0].RouteTableId" --output text)
+subnetId=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$vpcId" "Name=tag:Name,Values=my-$namePart-subnet-stemilia" --query "Subnets[0].SubnetId" --output text)
+rtId=$(aws ec2 describe-route-tables --filters "Name=vpc-id,Values=$vpcId" "Name=tag:Name,Values=my-$namePart-rt-stemilia" --query "RouteTables[0].RouteTableId" --output text)
 
 aws ec2 associate-route-table --route-table-id "$rtId" --subnet-id "$subnetId"
