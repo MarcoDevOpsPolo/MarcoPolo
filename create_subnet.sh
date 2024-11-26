2a918280ca9714d7e8b4379b5c542873b195f09a
Stefanov Emilia <stemilia92@gmail.com>
2024-11-26 13:18:50 +0300

    refactor making vpcId extraction automatic

--- create_subnet.sh
+++ create_subnet.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+vpcId=$(aws ec2 describe-vpcs --filters "Name=tag:Name,Values=my-vpc-stemilia" --query "Vpcs[0].VpcId" --output text
+)
 
 aws ec2 create-subnet \
-    --vpc-id vpc-09baa84c21a6b6703 \
+    --vpc-id "$vpcId" \
     --cidr-block 10.0.1.0/24 \
     --tag-specifications 'ResourceType=subnet,Tags=[{Key=Name,Value=my-private-subnet-stemilia}]'
