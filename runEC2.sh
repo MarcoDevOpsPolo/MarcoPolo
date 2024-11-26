2adad9a705ef1d30096ea532d9b4df709c50db53
Stefanov Emilia <stemilia92@gmail.com>
2024-11-26 14:01:12 +0300

    fix subnetID

--- runEC2.sh
+++ runEC2.sh
@@ -3,7 +3,7 @@
 imageID="ami-0b5673b5f6e8f7fa7" #region: frankfurt eu-central-1
 keyName="emi_rsa"    # personal key name
 vpcId=$(aws ec2 describe-vpcs --filters "Name=tag:Name,Values=my-vpc-stemilia" --query "Vpcs[0].VpcId" --output text)
-subnetID=$(aws ec2 describe-subnets --filters "Name=tag:name,Values=my-vpc-stemilia" --query "Subnets[0].SubnetId" --output text)
+subnetID=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$vpcId" --query "Subnets[0].SubnetId" --output text)
 secGroupId=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=default" "Name=vpc-id,Values=$vpcId" --query "SecurityGroups[0].GroupId" --output text)
 
 aws ec2 run-instances \
