2848499c512c5874a02fb528cfd19a1b62bd158a
Stefanov Emilia <stemilia92@gmail.com>
2024-11-26 14:59:55 +0300

    refactor create_subnet.sh to make it reusable

--- create_subnet.sh
+++ create_subnet.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+name="$1"
+cidr="$2" # e.g. 10.0.1.0/24
+
 vpcId=$(aws ec2 describe-vpcs --filters "Name=tag:Name,Values=my-vpc-stemilia" --query "Vpcs[0].VpcId" --output text
 )
 
 aws ec2 create-subnet \
     --vpc-id "$vpcId" \
-    --cidr-block 10.0.1.0/24 \
-    --tag-specifications 'ResourceType=subnet,Tags=[{Key=Name,Value=my-private-subnet-stemilia}]'
+    --cidr-block "$cidr" \
+    --tag-specifications "ResourceType=subnet,Tags=[{Key=Name,Value=$name}]"
