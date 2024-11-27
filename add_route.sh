744536bc2e71e04967785240301b0c08e96c53d7
Stefanov Emilia <stemilia92@gmail.com>
2024-11-27 09:58:22 +0300

    refactor add_route

--- add_route.sh
+++ add_route.sh
@@ -1,10 +1,35 @@
 #!/bin/bash
-vpcId=$(aws ec2 describe-vpcs --filters "Name=tag:Name,Values=my-vpc-stemilia" --query "Vpcs[0].VpcId" --output text
-)
-igwId=$(aws ec2 describe-internet-gateways --filters "Name=tag:Name,Values=my-igw-stemilia" --query "InternetGateways[0].InternetGatewayId" --output text)
-rtId=$(aws ec2 describe-route-tables --filters "Name=vpc-id,Values=$vpcId" "Name=tag:Name,Values=my-public-rt-stemilia" --query "RouteTables[0].RouteTableId" --output text)
 
-aws ec2 create-route \
-  --route-table-id "$rtId" \
-  --destination-cidr-block 0.0.0.0/0 \
-  --gateway-id "$igwId"
+rtNamePart="$1"
+
+# Get the VPC ID
+vpcId=$(aws ec2 describe-vpcs \
+  --filters "Name=tag:Name,Values=my-vpc-stemilia" \
+  --query "Vpcs[0].VpcId" \
+  --output text)
+
+# Get the Route Table ID
+rtId=$(aws ec2 describe-route-tables \
+  --filters "Name=vpc-id,Values=$vpcId" "Name=tag:Name,Values=my-$rtNamePart-rt-stemilia" \
+  --query "RouteTables[0].RouteTableId" \
+  --output text)
+
+# Prepare base command for adding a route
+addroutecmd="aws ec2 create-route --route-table-id \"$rtId\" --destination-cidr-block 0.0.0.0/0"
+
+# Check whether to use Internet Gateway or NAT Gateway
+if [[ "$rtNamePart" == "public" ]]; then
+  igwId=$(aws ec2 describe-internet-gateways \
+    --filters "Name=tag:Name,Values=my-igw-stemilia" \
+    --query "InternetGateways[0].InternetGatewayId" \
+    --output text)
+  addroutecmd="$addroutecmd --gateway-id $igwId"
+else
+  natgwId=$(aws ec2 describe-nat-gateways \
+    --query "NatGateways[0].NatGatewayId" \
+    --output text)
+  addroutecmd="$addroutecmd --gateway-id $natgwId"
+fi
+
+# Execute the final command
+eval "$addroutecmd"
