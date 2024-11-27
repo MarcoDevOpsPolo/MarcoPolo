700f0cf8f29879a186a6e1165b3b1e7a5b56538f
Stefanov Emilia <stemilia92@gmail.com>
2024-11-27 10:09:48 +0300

    edit_rt_tag_name.sh gets vpcId from parameter

--- edit_rt_tag_name.sh
+++ edit_rt_tag_name.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-vpcId=$(aws ec2 describe-vpcs --filters "Name=tag:Name,Values=my-vpc-stemilia" --query "Vpcs[0].VpcId" --output text
-)
+vpcId="$1"
 
 rtId=$(aws ec2 describe-route-tables --filters "Name=vpc-id,Values=$vpcId" --query "RouteTables[0].RouteTableId" --output text
 )
