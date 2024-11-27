9defe1df25ca20ba50083353be8415c711526a55
Stefanov Emilia <stemilia92@gmail.com>
2024-11-27 10:10:10 +0300

    create_subnet.sh gets vpcId from parameter

--- create_subnet.sh
+++ create_subnet.sh
@@ -2,8 +2,7 @@
 name="$1"
 cidr="$2" # e.g. 10.0.1.0/24
 
-vpcId=$(aws ec2 describe-vpcs --filters "Name=tag:Name,Values=my-vpc-stemilia" --query "Vpcs[0].VpcId" --output text
-)
+vpcId="$3"
 
 aws ec2 create-subnet \
     --vpc-id "$vpcId" \
