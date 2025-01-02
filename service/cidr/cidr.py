0fd82fa61e1cf0488ab260a1594136b8b190027b
majorosgereby <25921022+majorosgereby@users.noreply.github.com>
2025-01-02 18:48:44 +0100

    add regex checking for binary values

--- service/cidr/cidr.py
+++ service/cidr/cidr.py
@@ -2,12 +2,21 @@
 
 import optparse # for handling command line arguments (positional parameters)
 import math
-import sys
+import sys # for sys.argv
+import re # regex
 
 def ceil_log_2(n):
 	return math.ceil(math.log2(n))
 
+def binary_ipv4_to_octets(binIP):
+	binStrPattern=re.compile("^0b[01]+$")	# starts with 0b, followed by 0 or 1 until end of line
+	if not binStrPattern.match(str(binIP)):
+	  raise TypeError(str(binIP)+" provided is not in binary format")
+	print("great success")
+
+
 if __name__ == '__main__':
 	parser = optparse.OptionParser()
 	opts, args = parser.parse_args()
-	print(ceil_log_2(int(sys.argv[1])))
+	#print(ceil_log_2(int(sys.argv[1])))
+	binary_ipv4_to_octets(sys.argv[1])
