c0ac855cf5c3f992541f12e75f85afc5e205c182
Stefanov Emilia <stemilia92@gmail.com>
2025-01-31 11:12:43 +0300

    modify ssh.sh -> add 3.parameter

--- service/ssh.sh
+++ service/ssh.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+sshPath="$3"
 
 # Check if SSH agent is already running
 if ! grep -u "$USER" ssh-agent > /dev/null; then
     eval "$(ssh-agent)"
 fi
 
-ssh-add ~/Documents/CODECOOL/practices/DevOps/AWS/emi_rsa.pem
+ssh-add "$sshPath"
 
 #use ProxyJump -J 
 ssh -J ec2-user@"$1" ec2-user@"$2"
