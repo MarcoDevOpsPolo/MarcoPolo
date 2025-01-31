b6b3f3c89dafb2959b3086b473e48f3b3528251a
Stefanov Emilia <stemilia92@gmail.com>
2025-01-31 10:31:22 +0300

    modify associate_subnet_to_rt.sh

--- service/associate_subnet_to_rt.sh
+++ service/associate_subnet_to_rt.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 namePart="$1" #private or public
+rname="$3" #default: my-rt 
+sname="$4" #default: my-subnet
 vpcId="$2"
 
-subnetId=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$vpcId" "Name=tag:Name,Values=my-$namePart-subnet-stemilia" --query "Subnets[0].SubnetId" --output text)
-rtId=$(aws ec2 describe-route-tables --filters "Name=vpc-id,Values=$vpcId" "Name=tag:Name,Values=my-$namePart-rt-stemilia" --query "RouteTables[0].RouteTableId" --output text)
+subnetId=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$vpcId" "Name=tag:Name,Values=$namePart-$sname" --query "Subnets[0].SubnetId" --output text)
+rtId=$(aws ec2 describe-route-tables --filters "Name=vpc-id,Values=$vpcId" "Name=tag:Name,Values=$namePart-$rname" --query "RouteTables[0].RouteTableId" --output text)
 
 aws ec2 associate-route-table --route-table-id "$rtId" --subnet-id "$subnetId" >> /dev/null
