930ebeaf98b01063c7bb07bd85de1dce372bff08
Stefanov Emilia <stemilia92@gmail.com>
2025-01-31 10:22:25 +0300

    modify edit_rt_tag_name.sh to accept 2. parameter

--- service/edit_rt_tag_name.sh
+++ service/edit_rt_tag_name.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 vpcId="$1"
+rt_name="$2"
 
 rtId=$(aws ec2 describe-route-tables --filters "Name=vpc-id,Values=$vpcId" --query "RouteTables[0].RouteTableId" --output text
 )
 
-aws ec2 create-tags --resources "$rtId" --tags Key=Name,Value=my-private-rt-stemilia >> /dev/null
+aws ec2 create-tags --resources "$rtId" --tags Key=Name,Value="$rt_name" >> /dev/null
