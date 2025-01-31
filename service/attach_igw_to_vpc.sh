5a4313f93903614fc87a661df52ad40ce78c942e
Stefanov Emilia <stemilia92@gmail.com>
2025-01-31 10:36:24 +0300

    modify attach_igw_to_vpc.sh to accept 2. parameter

--- service/attach_igw_to_vpc.sh
+++ service/attach_igw_to_vpc.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 vpcId="$1"
-igwId=$(aws ec2 describe-internet-gateways --filters "Name=tag:Name,Values=my-igw-stemilia" --query "InternetGateways[0].InternetGatewayId" --output text)
+igwName="$2"
+igwId=$(aws ec2 describe-internet-gateways --filters "Name=tag:Name,Values=$igwName" --query "InternetGateways[0].InternetGatewayId" --output text)
 
 aws ec2 attach-internet-gateway \
     --internet-gateway-id "$igwId" \
