e66fb80d5a2a1d5e5ecca774216e2d8e8f5f30bd
Stefanov Emilia <stemilia92@gmail.com>
2025-01-31 11:01:50 +0300

    modify create_natgtw.sh

--- service/create_natgw.sh
+++ service/create_natgw.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 vpcId="$1"
-subnetId=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$vpcId" "Name=tag:Name,Values=my-public-subnet-stemilia" --query "Subnets[0].SubnetId" --output text)
+subnet="$2"
+natgtw="$3"
+subnetId=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$vpcId" "Name=tag:Name,Values=$subnet" --query "Subnets[0].SubnetId" --output text)
 
 
 aws ec2 create-nat-gateway \
     --subnet-id "$subnetId" \
     --allocation-id "$(aws ec2 allocate-address --query "AllocationId" --output text)" \
-    --tag-specifications "ResourceType=natgateway,Tags=[{Key=Name,Value=my-nat-gateway-stemilia}]" >> /dev/null
+    --tag-specifications "ResourceType=natgateway,Tags=[{Key=Name,Value=$natgtw}]" >> /dev/null
