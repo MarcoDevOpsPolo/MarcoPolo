065475dba99044c2190a6ef562b5c0bb987976e7
Stefanov Emilia <stemilia92@gmail.com>
2025-01-31 10:25:50 +0300

    modify create_rt.sh to accept 2. parameter

--- service/create_rt.sh
+++ service/create_rt.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 vpcId="$1"
+rtname="$2"
 
 aws ec2 create-route-table \
  --vpc-id "$vpcId" \
- --tag-specifications "ResourceType=route-table,Tags=[{Key=Name,Value=my-public-rt-stemilia}]" >> /dev/null
+ --tag-specifications "ResourceType=route-table,Tags=[{Key=Name,Value=$rtname}]" >> /dev/null
